654559dd578db9da3ba2790470b865bf9b667382
tcarver <tjc29@teak.dil.private.cimr.cam.ac.uk>
2015-10-30 14:36:13 +0000

    first commit

---
+++ test_monitor_sites.py
@@ -0,0 +1,18 @@
+import unittest
+import requests
+
+sites = ['http://www.chicp.org']
+
+
+class TestSites(unittest.TestCase):
+
+    def test_site(self):
+        for site in sites:
+            r = requests.get(site)
+            self.assertEqual(r.status_code, 200, site+' '+str(r.status_code))
+            r = requests.get(site+'/x')
+            self.assertEqual(r.status_code, 404, site+' '+str(r.status_code))
+            self.assertFalse("DEBUG = True" in r.content.decode("utf-8"), site+" DEBUG = True")
+
+if __name__ == '__main__':
+    unittest.main()
