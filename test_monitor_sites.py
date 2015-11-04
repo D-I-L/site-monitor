0fe2d083e161025d780875e444de6a772f84edfd
tcarver <tim.carver@cimr.cam.ac.uk>
2015-11-04 09:33:11 +0000

    allow for https

--- test_monitor_sites.py
+++ test_monitor_sites.py
@@ -2,18 +2,24 @@ import unittest
 import requests
 
 # web sites to monitor
-sites = ['http://www.chicp.org']
+sites = ['https://www.chicp.org']
 
 
 class TestSites(unittest.TestCase):
 
     def test_site(self):
         for site in sites:
-            r = requests.get(site)
+            r = self._get(site)
             self.assertEqual(r.status_code, 200, site+' '+str(r.status_code))
-            r = requests.get(site+'/x')
+            r = self._get(site+'/x')
             self.assertEqual(r.status_code, 404, site+' '+str(r.status_code))
             self.assertFalse("DEBUG = True" in r.content.decode("utf-8"), site+" DEBUG = True")
+    
+    def _get(self, url):
+        if url.startswith('https'):
+            return requests.get(url, verify=False)
+        else: 
+            return requests.get(url)
 
 if __name__ == '__main__':
     unittest.main()
